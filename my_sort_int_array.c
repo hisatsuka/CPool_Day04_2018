9fd079a65f81def2c7301ead2b5da49e33414044
Alexis Riot <alexis.riot@epitech.eu>
2018-10-04 17:29:14 +0200

    my_sort_int_array

---
+++ my_sort_int_array.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2018
+** my_sort_int_array
+** File description:
+** Sorts an integer array in ascending order
+*/
+
+void my_swap(int *a, int *b);
+
+void my_sort_int_array(int *array, int size)
+{
+    int check_size = 0;
+    int nb = 0;
+
+    while(check_size < size)
+    {
+        nb = 0;
+
+        while(nb < (size - 1))
+        {
+            if(array[nb] > array[nb + 1])
+            {
+                my_swap(&array[nb], &array[nb + 1]);
+            }
+            nb++;
+        }
+        check_size++;
+    }
+}
