070998669a03eb59f9fd78eb92961803fd2eb71d
Alexis Riot <alexis.riot@epitech.eu>
2018-10-05 15:54:15 +0200

    push

--- my_getnbr.c
+++ my_getnbr.c
@@ -5,6 +5,16 @@
 ** Show all integrers in char
 */
 
+int is_valid_int(long nb)
+{
+    if(nb > 2147483647 || nb <= -2147483648)
+        return (0);
+
+    else
+        return (nb);
+    //return ((nb > 2147483647 || nb < -2147483648) ? 1 : 0);
+}
+
 int my_getnbr(char const *str)
 {
     long nb = 0;
@@ -18,8 +28,16 @@ int my_getnbr(char const *str)
             if (str[i] >= '0' && str[i] <= '9')
                 nb = ((nb * 10) + (str[i] - '0'));
             if (!(str[i + 1] >= '0' && str[i + 1] <= '9'))
-                return (nb > 2147483647 || nb < -2147483648 ? 0 : (nb * specialchar));
+                return (is_valid_int(nb * specialchar));
+                //return (nb > 2147483647 || nb < -2147483648 ? 0 : (nb * specialchar));
         }
     }
-    return ((nb > 2147483647 || nb < -2147483648) ? 0 : (nb * specialchar));
+    return (is_valid_int(nb * specialchar));
+    //return ((nb > 2147483647 || nb < -2147483648) ? 0 : (nb * specialchar));
+}
+
+void main()
+{
+    printf("%d\n", my_getnbr("-2147483647"));
+    printf("%d\n", my_getnbr("-50"));
 }
